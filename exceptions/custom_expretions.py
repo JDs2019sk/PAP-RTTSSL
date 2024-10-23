307b5791d4a04b1addd43031222a2a0720ce66bd
JDs2019sk <joeldias1907@gmail.com>
2024-10-23 19:06:55 +0100

    custom expretions

---
+++ exceptions/custom_expretions.py
@@ -0,0 +1,7 @@
+class VideoDeviceError(Exception):
+    """Exception raised for video device related errors."""
+    pass
+
+class GestureDataError(Exception):
+    """Exception raised for gesture database related errors."""
+    pass
